7e753b262f6e429764295379f7ef5bc5d71daf0b
ramcharn010-del <ramcharn010@gmail.com>
2025-08-04 20:09:22 +0530

    rhino.380852

--- src/main/kotlin/Credentials.kt
+++ src/main/kotlin/Credentials.kt
@@ -1,4 +1,4 @@
 object Credentials {
-    const val USERNAME = "enter_your_instagram_username_here"
+    const val USERNAME = "enter_your_instagram_username_here" rhino.380852
     const val PASSWORD = "enter_your_instagram_password_here"
 }
