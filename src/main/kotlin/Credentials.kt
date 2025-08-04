bbd76536183d73384ea37a4b0dea0e9d0ef5f848
ramcharn010-del <ramcharn010@gmail.com>
2025-08-04 20:15:16 +0530

    Update Credentials.kt
    Chek password

--- src/main/kotlin/Credentials.kt
+++ src/main/kotlin/Credentials.kt
@@ -1,4 +1,21 @@
-object Credentials {
-    const val USERNAME = "enter_your_instagram_username_here"
-    const val PASSWORD = "enter_your_instagram_password_here"
-}
+https://jitpack.iorepositories {
+    ...
+    maven(url = "https://jitpack.io")
+}dependencies {
+    implementation("com.github.hadiyarajesh:insta-bot:Tag")
+}object Credentials {
+    const val USERNAME = mr om 
+    const val PASSWORD = "your_instagram_password"
+}Credentials.PASSWORDCredentials.USERNAME    val username = Credentials.USERNAME
+    val password = Credentials.PASSWORD
+
+    val bot = InstagramBot()
+    bot.prepare(username)
+    bot.login(username, password)// Get 100 followers of you
+bot.getSelfFollowers(100).collect { println(it) }
+// Like 5 medias from explore page
+bot.likeMediasByExplorePage(5).collect { println(it) }
+// Approve all pending follow requests
+bot.approveAllPendingFollowRequests().collect { println(it) }
+// Watch stories of 200 users based on given location
+bot.watchLocationUsersStories("enter_location_name_here", 200).collect { println(it) }
